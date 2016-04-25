79d418bd1411d009219514e0894d8f2461d5fc33
Laurent Raboteau <laurent.raboteau@taores.fr>
2016-04-25 15:22:54 +0200

    With Rails compass config.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  # Be carefull with it
   protect_from_forgery with: :exception
 end
